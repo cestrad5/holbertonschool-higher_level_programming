6bf8ef38328cc1f844c35f5e3dcc3e2afa9c675c
CAMILO <cestrad5@gmail.com>
2023-01-26 21:47:37 -0500

    Updating repo files

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for n in dir(hidden_4):
-        if not n.startswith("__"):
-            print(n)
+for i in range(0, 99):
+    print("{:d} = {}".format(i, hex(i)))
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i is not 89 and i / 10 < i % 10:
-        print("{:02d}".format(i), end=", ")
-    elif i is 89:
-        print("{:02d}".format(i))
+for i in range(0, 89):
+    if i / 10 < i % 10:
+        print(f"{i:02d}", end=', ')
+print(89)
