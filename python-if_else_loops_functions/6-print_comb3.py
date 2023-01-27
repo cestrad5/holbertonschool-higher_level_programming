e2000cfdc6396ed28ae2b0218d47104d15d0ef9e
CAMILO <cestrad5@gmail.com>
2023-01-26 22:01:08 -0500

    Task 6 solved

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{}, {}".format(i, j), end=' ')
-    print()
+for a in range(0, 10):
+    for b in range(0, 10):
+        if a != b and a < b:
+            print(f"{a}{b}", end="")
+            if a != 8:
+                print(", ", end="")
+print()
