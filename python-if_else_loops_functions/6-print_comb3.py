517f6f99aa8bf9f629f62018a22223af35e19192
CAMILO <cestrad5@gmail.com>
2023-01-26 21:52:25 -0500

    Task 6 solved

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 89):
-    if i / 10 < i % 10:
-        print(f"{i:02d}", end=', ')
-print(89)
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{}, {}".format(i, j), end=' ')
+    print()
