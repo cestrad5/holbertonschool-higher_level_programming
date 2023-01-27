491d3aec8eb7c5c6e14cf7d2bad9dff44dd3ebb1
CAMILO <cestrad5@gmail.com>
2023-01-27 18:12:49 -0500

    Testing solution for task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    from sys import argv
-    arguments = len(argv) - 1
-    if arguments is 0:
-        print("{:d} arguments.".format(arguments))
-    elif arguments is 1:
-        print("{:d} argument:".format(arguments))
-        print("{:d}: {}".format(1, argv[1]))
-    else:
-        print("{:d} arguments:".format(arguments))
-        for i in range(1, len(argv)):
-            print("{:d}: {}".format(i, argv[i]))
+    if len(argv) - 1 == 0:
+        print(f"0 arguments.")
+
+    elif len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+        print(f"{len(argv) - 1}: " + argv[1])
+
+    elif len(argv) > 2:
+        print(f"{len(argv) - 1} arguments:")
+        for a in range(1, len(argv)):
+            print(f"{a}: {argv[a]}")
