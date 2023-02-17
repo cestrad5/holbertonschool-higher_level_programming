580f12ade9353af5d1cdc2a1f0ec6a1db74fcd0e
CAMILO <cestrad5@gmail.com>
2023-02-17 06:21:30 -0500

    Solving the inport module at task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""function that returns True if the
-object is an instance of a class
-that inherited (directly or indirectly)
-from the specified class"""
+"""
+This function returns True if the object
+is an instance of a class that inherited
+(directly or indirectly) from the specified class,
+otherwise False.
+"""
 
 
 def inherits_from(obj, a_class):
-    """Checks if the object is an instance
+    """ inherit from a class
     """
     if type(obj) == a_class:
         return False
