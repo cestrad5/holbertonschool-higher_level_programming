af57e4915b46cdee9772167b4297ce9f224789e1
CAMILO <cestrad5@gmail.com>
2023-02-17 21:31:47 -0500

    Task 7 solved

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,18 +3,24 @@
     and then save them to a file"""
 
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    json_data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    with open("add_item.json", "w") as f:
-        f.write("[]")
-    if not sys.argv[1]:
-        exit(0)
-    json_data = load_from_json_file("add_item.json")
 
-for argv_data in sys.argv[1:]:
-    json_data.append(argv_data)
-save_to_json_file(json_data, "add_item.json")
+def add_item(filename=""):
+    """arguments to python list, next convert to json
+    and then write a file"""
+
+    try:
+        file_json = load_from_json_file(filename)
+    except Exception:
+        file_json = []
+
+    for i in sys.argv[1:]:
+        file_json.append(i)
+    save_to_json_file(file_json, filename)
+
+
+filename = "add_item.json"
+add_item(filename)
