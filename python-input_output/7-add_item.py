87a83259055465da99abaab93da22724f0dff23c
CAMILO <cestrad5@gmail.com>
2023-02-17 21:14:21 -0500

    Task 7 solved

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list, and then save them to a file"""
+"""Script that adds all arguments to a Python list,
+    and then save them to a file"""
 
-
-import sys
+import json
+from sys import argv
+from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 file_exists = exists("add_item.json")
 list = []
