b10f4d80658d6acd9b21d672dbee218d68e38e99
CAMILO <cestrad5@gmail.com>
2023-02-17 21:22:39 -0500

    Task 10 solved

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, wi=None):
+        """Method Returns: [type]: dict"""
+        if wi is None:
+            return self.__dict__
+        dict = {}
+        for i in wi:
+            if i in self.__dict__:
+                dict[i] = self.__dict__[i]
+        return dict
