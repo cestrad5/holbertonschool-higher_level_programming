01a692d356e40b44533a8d74d50092c115b0e7fe
CAMILO <cestrad5@gmail.com>
2023-03-17 05:26:25 -0500

    Task 0 Solved

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,16 @@
 Write a script that lists all states from the database hbtn_0e_0_usa:
 """
 
-
 from sys import argv
-
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    # Print results in comma delimited format
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    db.close()
+conn = MySQLdb.connect(host="localhost", port=3306,
+                       user="root", passwd="root", db="my_db", charset="utf8")
+cur = conn.cursor()
+# HERE I have to know SQL to grab all states in my database
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
