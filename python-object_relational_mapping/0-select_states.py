ae1daf82a982f64c10fe8df58d1e87e2afbfa2a4
CAMILO <cestrad5@gmail.com>
2023-03-17 05:39:38 -0500

    Task 0 Solved

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,32 @@
+
+
 #!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa:
+Your script should take 3 arguments:
+mysql username, mysql password and database name
+(no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
 """
-Write a script that lists all states from the database hbtn_0e_0_usa:
-"""
+
 
 from sys import argv
+
 import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user="root", passwd="root", db="my_db", charset="utf8")
-cur = conn.cursor()
-# HERE I have to know SQL to grab all states in my database
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-# Close all cursors
-cur.close()
-# Close all databases
-conn.close()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                           db=argv[3], port=3306, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    # Print results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Close cursors
+    cur.close()
+    # Close databases
+    conn.close()
